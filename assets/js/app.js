d25bf29f1630e1405a2ae96511f5f250ead621a0
Kamil Blanik <kamil.lukasz.blanik@gmail.com>
2020-08-28 23:32:25 +0200

    Added new home view

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,35 @@ const options = {
     threshold : 0.7
 };
 
+const texts = ['Kamil Błanik','Developer', 'Student', 'Astronomy lover', 'Gamer'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+    if(count === texts.length){
+        count = 0;
+    }
+
+    currentText = texts[count];
+
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+        setTimeout(type, 2500);
+    }
+
+    if(letter.length !== currentText.length){
+        setTimeout(type, 200);
+    }
+
+}());
+
 let observer = new IntersectionObserver(navCheck, options);
 
 function navCheck(entries){
